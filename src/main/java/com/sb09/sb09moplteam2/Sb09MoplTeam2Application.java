ef62bb3b71c7eb6b1a7c16fc2b7add58df695b75
Color-Marker <AAAstudying0987@gmail.com>
2026-06-22 13:17:06 +0900

    build: main 생성용 기본 프로젝트 커밋입니다.

---
+++ src/main/java/com/sb09/sb09moplteam2/Sb09MoplTeam2Application.java
@@ -0,0 +1,13 @@
+package com.sb09.sb09moplteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb09MoplTeam2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb09MoplTeam2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/sb09/sb09moplteam2/Sb09MoplTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb09.sb09moplteam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb09MoplTeam2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
