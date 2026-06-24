8db9795c32dc6f6e744dacc4aecc7110f8cdaa86
Color-Marker <AAAstudying0987@gmail.com>
2026-06-24 17:22:40 +0900

    fix: 사용하지 않는 메서드 삭제

--- src/main/java/com/sb09/sb09moplteam2/exception/notification/NotificationException.java
+++ src/main/java/com/sb09/sb09moplteam2/exception/notification/NotificationException.java
@@ -7,8 +7,4 @@ public class NotificationException extends MoplException {
   public NotificationException(ErrorCode errorCode) {
     super(errorCode);
   }
-
-  public NotificationException(ErrorCode errorCode, Throwable cause) {
-    super(errorCode, cause);
-  }
 }
