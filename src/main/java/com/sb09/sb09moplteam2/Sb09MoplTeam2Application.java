df5adfe888e0c3ff8c19c452039013919f1de712
unknown <cjhh0707@naver.com>
2026-06-25 17:45:14 +0900

    feat: 회원가입 단위 테스트

--- src/main/java/com/sb09/sb09moplteam2/Sb09MoplTeam2Application.java
+++ src/main/java/com/sb09/sb09moplteam2/Sb09MoplTeam2Application.java
@@ -2,9 +2,7 @@ package com.sb09.sb09moplteam2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Sb09MoplTeam2Application {
 
