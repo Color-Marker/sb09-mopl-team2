1667482c419c8e9f13fe00c8847df1a23f62db42
Newbress <75563523+Newbress@users.noreply.github.com>
2026-06-25 18:30:40 +0900

    Feat: Tmdb 응답 dto

---
+++ src/main/java/com/sb09/sb09moplteam2/content/batch/Tmdb/dto/TmdbMovieResponse.java
@@ -0,0 +1,13 @@
+package com.sb09.sb09moplteam2.content.batch.Tmdb.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record TmdbMovieResponse(
+    Long id,
+    String title,
+    String overview,
+    @JsonProperty("poster_path") String posterPath,
+    @JsonProperty("release_date") String releaseDate
+) {
+
+}
