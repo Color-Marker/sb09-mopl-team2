7bb7ba3c033e52f9a3c239492bf3eb492546a2e1
이용일 <lyi980403>
2026-06-25 17:49:45 +0900

    feat : WebSocket DTO 생성

---
+++ src/main/java/com/sb09/sb09moplteam2/websocket/dto/DirectMessageDto.java
@@ -0,0 +1,15 @@
+package com.sb09.sb09moplteam2.websocket.dto;
+
+import com.sb09.sb09moplteam2.dto.UserSummary;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record DirectMessageDto(
+    UUID id,
+    UUID conversationId,
+    Instant createdAt,
+    UserSummary sender,
+    UserSummary receiver,
+    String content
+) {}
