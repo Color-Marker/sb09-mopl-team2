606be2b168e8bfe0988375a49722aa9961ae2c8f
Color-Marker <AAAstudying0987@gmail.com>
2026-06-25 09:18:20 +0900

    feat: cursor 페이지네이션용 매퍼 구현 준비

---
+++ src/main/java/com/sb09/sb09moplteam2/notification/mapper/CursorResponseNotificationMapper.java
@@ -0,0 +1,10 @@
+package com.sb09.sb09moplteam2.notification.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CursorResponseNotificationMapper {
+
+}
