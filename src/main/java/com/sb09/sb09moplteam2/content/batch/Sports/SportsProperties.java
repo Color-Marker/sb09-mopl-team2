15ca3f051509db4dde9f4c9d88203ef27e4b8f6e
Newbress <75563523+Newbress@users.noreply.github.com>
2026-06-29 10:16:11 +0900

    Feat: Sports API 환경설정

--- src/main/java/com/sb09/sb09moplteam2/content/batch/Sports/SportsProperties.java
+++ src/main/java/com/sb09/sb09moplteam2/content/batch/Sports/SportsProperties.java
@@ -1,5 +1,6 @@
 package com.sb09.sb09moplteam2.content.batch.Sports;
 
-public class SportsProperties {
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
-}
+@ConfigurationProperties(prefix = "sports.api")
+public record SportsProperties(String key, String baseUrl) {}
