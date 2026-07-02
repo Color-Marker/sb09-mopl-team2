21a187c272f7028fc2532ee3e79fa999957b6447
Color-Marker <AAAstudying0987@gmail.com>
2026-07-02 19:11:05 +0900

    fix: docker compose 진행 시 터지는 부분 일괄 수정

--- src/main/java/com/sb09/sb09moplteam2/exception/ErrorCode.java
+++ src/main/java/com/sb09/sb09moplteam2/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
   USER_NOT_FOUND("사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   DUPLICATE_USER("이미 존재하는 사용자입니다.", HttpStatus.CONFLICT),
   INVALID_USER_CREDENTIALS("잘못된 사용자 인증 정보입니다.", HttpStatus.UNAUTHORIZED),
+  USER_FORBIDDEN("접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
 
   // Server 에러 코드
   INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
