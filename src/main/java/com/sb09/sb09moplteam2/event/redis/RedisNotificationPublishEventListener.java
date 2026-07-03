033857d463dacd86413abd37817081b1c5dd8627
Color-Marker <AAAstudying0987@gmail.com>
2026-07-03 10:39:07 +0900

    feat: redis 적용 준비

---
+++ src/main/java/com/sb09/sb09moplteam2/event/redis/RedisNotificationPublishEventListener.java
@@ -0,0 +1,6 @@
+package com.sb09.sb09moplteam2.event.redis;
+
+public class RedisNotificationPublishEventListener {
+  // redis 쪽 작동 시키는 sse 서비스 호출하는 거 적을 예정
+  // 이거 합쳐지는 데로 sse 파트 브랜치에 ㅈㄱ업할 겁니다.
+}
