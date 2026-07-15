5663ed31ee4c56e38dc37b048a2e0585f9096db5
Newbress <75563523+Newbress@users.noreply.github.com>
2026-07-15 15:49:04 +0900

    Fix: 이미 색인된 데이터 건너뛰기 추가

--- src/main/java/com/sb09/sb09moplteam2/content/search/ContentSearchInitializer.java
+++ src/main/java/com/sb09/sb09moplteam2/content/search/ContentSearchInitializer.java
@@ -16,12 +16,19 @@ public class ContentSearchInitializer {
 
   private final ContentRepository contentRepository;
   private final ContentSearchService contentSearchService;
+  private final ContentSearchRepository contentSearchRepository;
 
   @EventListener(ApplicationReadyEvent.class)
   public void reindexAll() {
+    long existingCount = contentSearchRepository.count();
+    if (existingCount > 0) {
+      log.info("Elasticsearch에 이미 색인된 데이터가 있어 재색인을 건너뜁니다 - {}건", existingCount);
+      return;
+    }
+
     List<Content> allContents = contentRepository.findAll();
     allContents.forEach(content ->
         contentSearchService.index(ContentDocument.from(content)));
-    log.info("Elasticsearch 초기 색인 완료 - {}건", allContents.size());
+    log.info("Elasticsearch 색인 완료 - {}건", allContents.size());
   }
 }
