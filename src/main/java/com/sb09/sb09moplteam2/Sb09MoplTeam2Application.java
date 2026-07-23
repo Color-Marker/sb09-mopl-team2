55b176c7ca322047ed65482f9a8adfbc66d8759b
Color-Marker <AAAstudying0987@gmail.com>
2026-07-23 09:56:45 +0900

    refactor: opensearch 방식으로 변경. 로컬 도커 작동 확인

--- src/main/java/com/sb09/sb09moplteam2/Sb09MoplTeam2Application.java
+++ src/main/java/com/sb09/sb09moplteam2/Sb09MoplTeam2Application.java
@@ -4,12 +4,13 @@ import com.sb09.sb09moplteam2.content.batch.sport.SportProperties;
 import com.sb09.sb09moplteam2.content.batch.tmdb.TmdbProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry
-@SpringBootApplication
+@SpringBootApplication(exclude = { ElasticsearchDataAutoConfiguration.class })
 @EnableScheduling
 @EnableConfigurationProperties({TmdbProperties.class, SportProperties.class})
 public class Sb09MoplTeam2Application {
