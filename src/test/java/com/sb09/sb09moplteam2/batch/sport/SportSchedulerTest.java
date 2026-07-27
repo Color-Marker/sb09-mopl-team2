dcf8b8d785ab9e27f020373702edd700c9c5cdca
Newbress <75563523+Newbress@users.noreply.github.com>
2026-07-27 11:14:38 +0900

    Test: impport 제거

--- src/test/java/com/sb09/sb09moplteam2/batch/sport/SportSchedulerTest.java
+++ src/test/java/com/sb09/sb09moplteam2/batch/sport/SportSchedulerTest.java
@@ -20,11 +20,10 @@ import org.redisson.api.RedissonClient;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
+
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
+
 
 @ExtendWith(MockitoExtension.class)
 class SportSchedulerTest {
